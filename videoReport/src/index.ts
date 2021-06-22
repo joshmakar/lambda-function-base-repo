29e52b19f8f9fb4146281130b7049c052583dcd8
smithdalec <smithdalec@gmail.com>
2021-06-22 18:59:44 -0400

    Add ts and deps

--- videoReport/src/index.ts
+++ videoReport/src/index.ts
@@ -1,6 +1,6 @@
 console.log('Loading function');
 
-exports.handler = async (event, context) => {
+export async function handler(event: {}, context?: {}) {
     console.log('This code is from github!');
     console.log('event =', JSON.stringify(event, null, 2));
     console.log('context =', JSON.stringify(context, null, 2));
