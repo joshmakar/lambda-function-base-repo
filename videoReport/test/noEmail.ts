7249e5e6e0da16db32b2faba02abe72182dd3c84
Josh Makar <josh.makar@myshopmanager.com>
2021-06-30 16:53:15 -0400

    Mov dealerids to var to make easier to add/remove

--- videoReport/test/noEmail.ts
+++ videoReport/test/noEmail.ts
@@ -4,6 +4,11 @@ import { handler } from '../src/index'
 // Load env vars from .env
 config({ path: __dirname + '/../../.env' })
 
+const dealerIds = [
+    "bdc52a27-09d9-0384-f719-5744655473c3",
+    "1f6f005005af11e888ba126446333acf",
+];
+
 // Invoke the lambda entrypoint for Parker Audi and Audi North Park
-handler({ dealerIDs: ["bdc52a27-09d9-0384-f719-5744655473c3", "1f6f005005af11e888ba126446333acf"] })
+handler({ dealerIDs: dealerIds })
     .then(resp => console.log('Lambda fn completed with response:', resp))
