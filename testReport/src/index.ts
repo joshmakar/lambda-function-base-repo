82153444116ca198646b77f92a9a22ada479dbaa
Josh Makar <josh.makar@myshopmanager.com>
2021-07-02 12:05:15 -0400

    Format response

--- testReport/src/index.ts
+++ testReport/src/index.ts
@@ -2,5 +2,14 @@
  * This is the entry point for the Lambda function
  */
 export async function handler(event: {}) {
-    return { message: 'Hello World' };
+    const response = {
+        "statusCode": 200,
+        "headers": {
+            "my_header": "my_value"
+        },
+        "body": { message: 'Hello World' },
+        "isBase64Encoded": false
+    };
+
+    return response;
 };
