0fc78dca701b181219e84dfe8ed6737d097cee38
joshmakar <9956440+joshmakar@users.noreply.github.com>
2022-04-27 10:01:30 -0400

    Get Lambda to a working state

--- toyotaRecallReports/src/index.ts
+++ toyotaRecallReports/src/index.ts
@@ -2,6 +2,7 @@
 import mysql from 'promise-mysql';
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { createObjectCsvStringifier } from 'csv-writer';
+import axios from 'axios';
 
 // Import query functions
 import {
@@ -32,122 +33,147 @@ if (process.env['NODE_ENV'] !== 'production') {
  * The main function that runs the entire process.
  */
 const handler = async (event: any, _context: any, callback: any) => {
-  console.log('Received event:', JSON.stringify(event, null, 2));
-  // Check required DB connection environment variables
-  ['UNOTIFI_API_TOKEN'].forEach(envVar => {
-    if (!process.env[envVar]) {
-      callback(`Please set ${envVar} in your environment`);
+  try {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    // Check required DB connection environment variables
+    ['UNOTIFI_API_TOKEN', 'UNOTIFI_REPORTS_BUCKET'].forEach(envVar => {
+      if (!process.env[envVar]) {
+        callback(`Please set ${envVar} in your environment`);
+        return;
+      }
+    });
+
+    event = JSON.parse(event.Records[0].body);
+
+    // Check that the event contains one or more dealershipIntegralinkCodes
+    if (!event.dealershipIntegralinkCodes?.length) {
+      callback('Please provide at least one dealershipIntegralinkCode');
       return;
     }
-  });
 
-  event = JSON.parse(event.body);
+    // Check that the event contains a startDate and endDate
+    if (!event.startDate || !event.endDate) {
+      callback('Please provide a startDate and endDate');
+      return;
+    }
 
-  // Check that the event contains one or more dealershipIntegralinkCodes
-  if (!event.dealershipIntegralinkCodes?.length) {
-    callback('Please provide at least one dealershipIntegralinkCode');
-    return;
-  }
+    // Check that the even has a replyTo field
+    if (!event.replyTo) {
+      callback('Please provide a replyTo field');
+      return;
+    }
 
-  // Check that the event contains a startDate and endDate
-  if (!event.startDate || !event.endDate) {
-    callback('Please provide a startDate and endDate');
-    return;
-  }
+    // Get dealerships db info
+    const unotifiApiClient = new UnotifiApiClient(process.env['UNOTIFI_API_TOKEN']!);
+    const dealershipsConnections: DealershipDBInfo[] = await unotifiApiClient.getDealershipsDBInfo(event.dealershipIntegralinkCodes);
 
-  // Get dealerships db info
-  const unotifiApiClient = new UnotifiApiClient(process.env['UNOTIFI_API_TOKEN']!);
-  const dealershipsConnections: DealershipDBInfo[] = await unotifiApiClient.getDealershipsDBInfo(event.dealershipIntegralinkCodes);
+    // Convert string date to Date object
+    const startDate: Date = new Date(event.startDate);
+    const endDate: Date = new Date(event.endDate);
 
-  // Convert string date to Date object
-  const startDate: Date = new Date(event.startDate);
-  const endDate: Date = new Date(event.endDate);
+    // Get report data for each dealership
+    const dealershipsResults = await Promise.all(
+      dealershipsConnections.map((connection) => {
+        console.log(JSON.stringify(connection));
+        return getReportData(connection, startDate, endDate);
+      })
+    );
 
-  // Get report data for each dealership
-  const dealershipsResults = await Promise.all(
-    dealershipsConnections.map((connection) => {
-      console.log(JSON.stringify(connection));
-      return getReportData(connection, startDate, endDate);
-    })
-  );
+    console.log('Finished getting report data.');
 
-  console.log('Finished getting report data.');
+    const resultsFormatted: FormattedResult[] = [];
 
-  const resultsFormatted: FormattedResult[] = [];
-
-  // Format the results
-  dealershipsResults.forEach((results: ReturnedResult[]) => {
-    results.forEach((result: ReturnedResult) => {
-      resultsFormatted.push({
-        autoCampaignName: result.autoCampaignName,
-        dealershipName: result.dealershipName,
-        revenue: result.revenue ?? 0,
-        soldVehicles: result.soldVehicles ?? 0,
-        totalAppointments: result.totalAppointments ?? 0,
-        totalAppointmentsArrived: result.totalAppointmentsArrived ?? 0,
-        totalOpportunities: result.totalOpportunities ?? 0,
-        totalOpportunitiesCalled: result.totalOpportunitiesCalled ?? 0,
-        totalOpportunitiesContacted: result.totalOpportunitiesContacted ?? 0,
-        totalOpportunitiesTexted: result.totalOpportunitiesTexted ?? 0,
-        totalRepairOrders: result.totalRepairOrders ?? 0,
+    // Format the results
+    dealershipsResults.forEach((results: ReturnedResult[]) => {
+      results.forEach((result: ReturnedResult) => {
+        resultsFormatted.push({
+          autoCampaignName: result.autoCampaignName,
+          dealershipName: result.dealershipName,
+          revenue: result.revenue ?? 0,
+          soldVehicles: result.soldVehicles ?? 0,
+          totalAppointments: result.totalAppointments ?? 0,
+          totalAppointmentsArrived: result.totalAppointmentsArrived ?? 0,
+          totalOpportunities: result.totalOpportunities ?? 0,
+          totalOpportunitiesCalled: result.totalOpportunitiesCalled ?? 0,
+          totalOpportunitiesContacted: result.totalOpportunitiesContacted ?? 0,
+          totalOpportunitiesTexted: result.totalOpportunitiesTexted ?? 0,
+          totalRepairOrders: result.totalRepairOrders ?? 0,
+        });
       });
     });
-  });
 
-  const csvWriterResults = createObjectCsvStringifier({
-    header: [
-      { id: 'dealershipName', title: 'Dealership Name' },
-      { id: 'autoCampaignName', title: 'Campaign Name' },
-      { id: 'totalOpportunities', title: 'Total Opportunities' },
-      { id: 'totalOpportunitiesContacted', title: 'Total Opportunities Contacted' },
-      { id: 'totalOpportunitiesTexted', title: 'Total Opportunities Texted' },
-      { id: 'totalOpportunitiesCalled', title: 'Total Opportunities Called' },
-      { id: 'totalAppointments', title: 'Total Appointments' },
-      { id: 'totalAppointmentsArrived', title: 'Total Appointments Arrived' },
-      { id: 'totalRepairOrders', title: 'Total Repair Orders' },
-      { id: 'revenue', title: 'Revenue' },
-      { id: 'soldVehicles', title: 'Sold Vehicles' },
-    ]
-  });
+    const csvWriterResults = createObjectCsvStringifier({
+      header: [
+        { id: 'dealershipName', title: 'Dealership Name' },
+        { id: 'autoCampaignName', title: 'Campaign Name' },
+        { id: 'totalOpportunities', title: 'Total Opportunities' },
+        { id: 'totalOpportunitiesContacted', title: 'Total Opportunities Contacted' },
+        { id: 'totalOpportunitiesTexted', title: 'Total Opportunities Texted' },
+        { id: 'totalOpportunitiesCalled', title: 'Total Opportunities Called' },
+        { id: 'totalAppointments', title: 'Total Appointments' },
+        { id: 'totalAppointmentsArrived', title: 'Total Appointments Arrived' },
+        { id: 'totalRepairOrders', title: 'Total Repair Orders' },
+        { id: 'revenue', title: 'Revenue' },
+        { id: 'soldVehicles', title: 'Sold Vehicles' },
+      ]
+    });
 
-  const csvData = csvWriterResults.getHeaderString() + csvWriterResults.stringifyRecords(resultsFormatted);
+    const csvData = csvWriterResults.getHeaderString() + csvWriterResults.stringifyRecords(resultsFormatted);
 
-  const s3Key = generateS3Key('toyota-recall-reports', 'csv', { prependToPath: 'Toyota_Recall_Reports' });
-  console.log(`S3 Key: ${s3Key}`);
+    const s3Key = generateS3Key('toyota-recall-reports', 'csv', { prependToPath: 'Toyota_Recall_Reports' });
+    console.log(`S3 Key: ${s3Key}`);
 
-  // This works when running via nodejs
-  // const s3 = new S3Client({
-  //   region: 'us-east-1', // The value here doesn't matter.
-  //   endpoint: 'http://localhost:4566', // This is the localstack EDGE_PORT
-  //   forcePathStyle: true
-  // });
+    // This works when running via nodejs
+    // const s3 = new S3Client({
+    //   region: 'us-east-1', // The value here doesn't matter.
+    //   endpoint: 'http://localhost:4566', // This is the localstack EDGE_PORT
+    //   forcePathStyle: true
+    // });
 
-  // This works when running via lambda
-  console.log('Connecting to S3...');
-  const s3 = new S3Client({
-    region: 'us-east-1', // The value here doesn't matter.
-    // endpoint: 'http://172.17.0.2:4566', // This is the localstack EDGE_PORT
-    forcePathStyle: true
-  });
-  console.log('Connected to S3.');
+    // This works when running via lambda
+    console.log('Connecting to S3...');
+    const s3 = new S3Client({
+      region: 'us-east-1', // The value here doesn't matter.
+      // endpoint: 'http://172.17.0.2:4566', // This is the localstack EDGE_PORT
+      forcePathStyle: true
+    });
+    console.log('Connected to S3.');
 
-  // Save the csv data to S3
-  console.log('Saving CSV data to S3...');
-  await s3.send(new PutObjectCommand({
-    Bucket: 'unotifi-reports',
-    Key: s3Key,
-    Body: csvData,
-    ContentType: 'text/csv',
-  }));
-  console.log('Saved CSV data to S3.');
+    // Save the csv data to S3
+    console.log('Saving CSV data to S3...');
+    await s3.send(new PutObjectCommand({
+      Bucket: process.env['UNOTIFI_REPORTS_BUCKET'],
+      Key: s3Key,
+      Body: csvData,
+      ContentType: 'text/csv',
+    }));
+    console.log('Saved CSV data to S3.');
 
-  callback(null, {
-    statusCode: 201,
-    body: s3Key,
-    headers: {
-      'X-Custom-Header': 'ASDF'
-    }
-  });
+    console.log('Updating report', event.replyTo);
+    await axios.put(event.replyTo, {
+        status: 'completed',
+        csv_s3_bucket: process.env['UNOTIFI_REPORTS_BUCKET'],
+        csv_s3_key: s3Key,
+      })
+      .then((response) => {
+        console.log('Updated report', response.data);
+      })
+      .catch((error) => {
+        console.log('Error updating report', error);
+        throw new Error("Error updating report", error);
+      });
+
+    callback(null, {
+      statusCode: 201,
+      body: s3Key,
+      headers: {
+        'X-Custom-Header': 'ASDF'
+      }
+    });
+  } catch (error) {
+    console.log('Error:', error);
+    callback(error);
+  }
 }
 
 module.exports = {
@@ -239,6 +265,7 @@ async function getReportData(dealershipDBInfo: DealershipDBInfo, startDate: Date
 //   // dealershipIntegralinkCodes: ['e108cd88-bea5-f4af-11ac-574465d1fd2f', 'c5930e0c-72d6-4cd4-bfdf-d74db1d0ce38'],
 //   startDate: startDate,
 //   endDate: endDate,
+//   replyTo: 'https://webhook.site/74251db4-e0ee-4151-9ab5-33d998362a01',
 // };
 
 // handler(event, {}, (error: any, response: any) => {
