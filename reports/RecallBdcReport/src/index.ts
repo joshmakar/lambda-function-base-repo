723a4484a1f71a1c841fe8c731b09dc0d26c6f4d
joshmakar <9956440+joshmakar@users.noreply.github.com>
2022-05-03 14:56:44 -0400

    Add way to test locally

--- reports/RecallBdcReport/src/index.ts
+++ reports/RecallBdcReport/src/index.ts
@@ -222,29 +222,3 @@ async function getReportData(dealershipDBInfo: DealershipDBInfo, startDate: Date
 
   return results;
 }
-
-// /**
-// * Local testing
-// */
-// // const startDate = new Date('2021-12-01');
-// // const endDate = new Date('2021-12-31');
-// const startDate = new Date();
-// startDate.setFullYear(startDate.getFullYear() - 2);
-// const endDate = new Date();
-// // const startDate = new Date();
-// // startDate.setDate(startDate.getDate() - 90);
-// // const endDate = new Date();
-
-// const event: Event = {
-//   // dealershipIntegralinkCodes: ['e108cd88-bea5-f4af-11ac-574465d1fd2f'],
-//   // dealershipIntegralinkCodes: ['c5930e0c-72d6-4cd4-bfdf-d74db1d0ce38'],
-//   dealershipIntegralinkCodes: ['99999'],
-//   // dealershipIntegralinkCodes: ['e108cd88-bea5-f4af-11ac-574465d1fd2f', 'c5930e0c-72d6-4cd4-bfdf-d74db1d0ce38'],
-//   startDate: startDate,
-//   endDate: endDate,
-//   replyTo: 'https://webhook.site/74251db4-e0ee-4151-9ab5-33d998362a01',
-// };
-
-// handler(event, {}, (error: any, response: any) => {
-//   return response ? console.log('Response:', response) : console.log('Error:', error);
-// });
